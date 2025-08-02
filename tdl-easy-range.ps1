b1b9a27378de7fb4794e7e0141f292a196698667
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2025-08-02 19:23:22 +0300

    Update tdl-easy-range.ps1

--- tdl-easy-range.ps1
+++ tdl-easy-range.ps1
@@ -159,7 +159,7 @@ if (-not $useSaved) {
             }
             if (-not $input.EndsWith("/")) { $input = "$input/" }
             if ($input -notmatch '^https?://t\.me/(?:(?:c/\d+/\d+)|(?:s/[A-Za-z0-9_]{5,32}/\d+)|(?:[A-Za-z0-9_]{5,32}/\d+))/?$') {
-                Write-Host "🔴 Error: URL must be of form https://t.me/c/12345678/ or https://t.me/abc/123." -ForegroundColor Red
+                Write-Host "🔴 Error: URL must be of form https://t.me/c/12345678/123 or https://t.me/abc/123." -ForegroundColor Red
                 continue
             }
             $telegramUrl = $input
